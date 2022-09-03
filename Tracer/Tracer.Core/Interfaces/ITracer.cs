04f0bbbc32972f45685087f3417d00e76168cb2f
boy-eff <boyev.danila@gmail.com>
2022-09-03 18:04:49 +0300

    Add basic project structure

---
+++ Tracer/Tracer.Core/Interfaces/ITracer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracer.Core.Interfaces
+{
+    public interface ITracer 
+    {
+        // вызывается в начале замеряемого метода
+        void StartTrace();
+
+        // вызывается в конце замеряемого метода
+        void StopTrace();
+
+        // получить результаты измерений
+        TraceResult GetTraceResult();
+    }
+}
---
+++ Tracer/Tracer.Example/Fakes/FakeClass.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracer.Example.Fakes
+{
+    public class FakeClass
+    {
+        private ITracer _tracer;
+    
+        public FakeClass(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void M0()
+        {
+            M1();
+            M2();
+        }
+        
+        private void M1()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(100);
+            _tracer.StopTrace();
+        }
+        
+        private void M2()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(200);
+            _tracer.StopTrace();
+        }
+    }
+}
