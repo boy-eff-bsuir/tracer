9c91b8349e98dea6a513a581ea313c1f888d292a
boy-eff <boyev.danila@gmail.com>
2022-09-04 22:02:11 +0300

    Add references to projects

---
+++ Serialization/Json/JsonTraceResultSerializer.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tracer.Serialization.Abstractions;
+using Tracer.Core.Models;
+
+
+
+namespace Json
+{
+    public class JsonTraceResultSerializer : ITraceResultSerializer
+    {
+        void Serialize(TraceResult traceResult, Stream to)
+        {
+
+        }
+    }
+}
